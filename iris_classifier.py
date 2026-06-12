8debb8789f76a2d0fd4e50da9b5d2932a47bd438
anuja y <anujaay013@gmail.com>
2026-06-12 14:15:22 +0530

    Add files via upload

---
+++ iris_classifier.py
@@ -0,0 +1,23 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+iris = load_iris()
+X = iris.data
+y = iris.target
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,
+    random_state=42
+)
+scaler = StandardScaler()
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+model = KNeighborsClassifier(n_neighbors=5)
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+accuracy = accuracy_score(y_test, predictions)
+print("AI Flower Classification Model")
+print("Accuracy:", round(accuracy * 100, 2), "%")
